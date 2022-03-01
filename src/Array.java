96527e334708199a82a0ccf6be18f8cda81e4932
yasyaindra <indrayasya96@gmail.com>
2022-03-02 00:58:31 +0800

    :fire: operation

--- src/Array.java
+++ src/Array.java
@@ -3,5 +3,21 @@ public class Array {
 //        String[] arrayString;
 //        arrayString = new String[3];
 //        arrayString[0] = "Indra";
+//        int[] arrayAngka = new int[]{
+//                10,23,13,18,8
+//        };
+//
+//        System.out.println(arrayAngka[0]);
+//        long[] arrayLong = {
+//                10,23,13,18,8
+//        };
+//        System.out.println(arrayLong[3]);
+//        Array dalam Array
+//        String[][][] members = {
+//                {{"Indra","Maulana","Yasya"},{"Afifan","Hakim","Firdaus"},{"Idris","Wahyu","Ramdahan"}},
+//                {{"Indra","Maulana","Yasya"},{"Afifan","Hakim","Firdaus"},{"Idris","Wahyu","Ramdahan"}},
+//                {{"Indra","Maulana","Yasya"},{"Afifan","Hakim","Firdaus"},{"Idris","Wahyu","Ramdahan"}},
+//        };
+//        System.out.println(members[2][1][2]);
     }
 }
---
+++ src/Operation.java
@@ -0,0 +1,7 @@
+public class Operation {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 4;
+        System.out.println(b!=a);
+    }
+}
